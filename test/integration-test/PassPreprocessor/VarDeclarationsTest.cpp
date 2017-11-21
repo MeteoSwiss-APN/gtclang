0c6f0417dc36a828f5c175a02b0c809caa6df2f9
Tobias Wicky <twicki@ethz.ch>
2017-11-21 14:59:55 +0100

    integration test for var declarations

---
+++ test/integration-test/PassPreprocessor/VarDeclarationsTest.cpp
@@ -0,0 +1,45 @@
+//===--------------------------------------------------------------------------------*- C++ -*-===//
+//                         _       _
+//                        | |     | |
+//                    __ _| |_ ___| | __ _ _ __   __ _
+//                   / _` | __/ __| |/ _` | '_ \ / _` |
+//                  | (_| | || (__| | (_| | | | | (_| |
+//                   \__, |\__\___|_|\__,_|_| |_|\__, | - GridTools Clang DSL
+//                    __/ |                       __/ |
+//                   |___/                       |___/
+//
+//
+//  This file is distributed under the MIT License (MIT).
+//  See LICENSE.txt for details.
+//
+//===------------------------------------------------------------------------------------------===//
+
+// RUN: %gtclang% %file% -fno-codegen -freport-pass-preprocessor
+
+
+#include "gridtools/clang_dsl.hpp"
+
+using namespace gridtools::clang;
+
+stencil Test01 {
+  storage foo;
+  var a;
+
+  void Do() {
+    vertical_region(k_start, k_end) {
+      var b = foo;
+      var c = 10;
+    }
+    vertical_region(k_start, k_end) {
+      a = foo;
+      var d, e = foo;
+      d = 10;
+      foo = e + d;
+    }
+    vertical_region(k_start, k_end) {
+      var b, c = foo;
+      b = a;
+      foo = c + b;
+    }
+  }
+};
