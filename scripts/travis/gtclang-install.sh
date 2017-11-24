6f491833f5f53c0701f0ce79527a93d12b86d9bc
carlos osuna <carlos.osuna@meteoswiss.ch>
2017-11-24 18:57:07 +0100

    fix script

--- scripts/travis/gtclang-install.sh
+++ scripts/travis/gtclang-install.sh
@@ -19,6 +19,9 @@ this_script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 gtclang_install_dependencies() {
 
+  export DAWN_DIR="$CACHE_DIR/dawn"
+  export DAWN_SCRIPT_DIR="$DAWN_DIR/scripts/travis"
+
   # Install 3rd party dependencies
   source "$DAWN_SCRIPT_DIR/install.sh"
   install_driver -i ${CACHE_DIR} -b cmake,protobuf,boost -c boost:system
