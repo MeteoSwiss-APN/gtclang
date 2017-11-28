f4136de80f760b6552882a23fdce676d6e4e4605
carlos osuna <carlos.osuna@meteoswiss.ch>
2017-11-28 23:25:48 +0100

    fix in script

--- scripts/travis/gtclang-driver-test.sh
+++ scripts/travis/gtclang-driver-test.sh
@@ -48,9 +48,8 @@ cmake .. -DCMAKE_CXX_COMPILER="$CXX"                                            
          -DGTCLANG_TESTING=ON                                                                                                 \
          -DGTCLANG_UNIT_TESTING=ON                                                                                            \
          -DGTCLANG_INTEGRATION_TESTING=ON                                                                                     \
-         -DPYTHON_EXECUTABLE="$PYTHON_DIR/bin/python3"                                                                        \
-         -DProtobuf_DIR=${build_dir}/dawn-prefix/src/dawn-build/protobuf-prefix/src/protobuf-build/lib/cmake/protobuf/        \
          -DDawn_DIR=${build_dir}/dawn-prefix/src/dawn-build/prefix/dawn/cmake/                                                \
+         -DPYTHON_EXECUTABLE="$PYTHON_DIR/bin/python3"                                                                        \
       || fatal_error "failed to configure"
 
 make -j2 || fatal_error "failed to build"
