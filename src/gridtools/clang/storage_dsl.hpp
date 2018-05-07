238929a18f7e5e7ffd3105e0af43ab29fa2127c6
Tobias Wicky <twicki@ethz.ch>
2018-05-07 15:07:54 +0200

    fields with directions
    initial commit of fields with directions

--- src/gridtools/clang/storage_dsl.hpp
+++ src/gridtools/clang/storage_dsl.hpp
@@ -83,6 +83,390 @@ struct storage {
   operator double() const;
 };
 
+/**
+ * @brief Dummy 1-dimensional storage
+ * @ingroup gridtools_clang
+ */
+struct storage_i : public storage {
+  storage_i();
+
+  storage_i& operator=(const storage&);
+  storage_i& operator+=(const storage&);
+  storage_i& operator-=(const storage&);
+  storage_i& operator/=(const storage&);
+  storage_i& operator*=(const storage&);
+
+  storage_i& operator()(int);
+
+  /**
+   * @name 1D access
+   * @{
+   */
+  storage_i& operator()(direction);
+  storage_i& operator()(dimension);
+  storage_i& operator()(offset);
+  /** @} */
+
+  /**
+   * @name 2D access
+   * @{
+   */
+
+  storage_i& operator()(dimension, dimension);
+
+  storage_i& operator()(dimension, direction);
+  storage_i& operator()(direction, dimension);
+  storage_i& operator()(direction, direction);
+  /** @} */
+
+  /**
+   * @name 3D access
+   * @{
+   */
+  storage_i& operator()(dimension, dimension, dimension);
+
+  storage_i& operator()(direction, direction, direction);
+  storage_i& operator()(dimension, direction, direction);
+  storage_i& operator()(direction, dimension, direction);
+  storage_i& operator()(direction, direction, dimension);
+  storage_i& operator()(dimension, dimension, direction);
+  storage_i& operator()(dimension, direction, dimension);
+  storage_i& operator()(direction, dimension, dimension);
+  /** @} */
+
+  operator double() const;
+};
+
+/**
+ * @brief Dummy 1-dimensional storage
+ * @ingroup gridtools_clang
+ */
+struct storage_j : public storage {
+  storage_j();
+
+  storage_j& operator=(const storage&);
+  storage_j& operator+=(const storage&);
+  storage_j& operator-=(const storage&);
+  storage_j& operator/=(const storage&);
+  storage_j& operator*=(const storage&);
+
+  storage_j& operator()(int);
+
+  /**
+   * @name 1D access
+   * @{
+   */
+  storage_j& operator()(direction);
+  storage_j& operator()(dimension);
+  storage_j& operator()(offset);
+  /** @} */
+
+  /**
+   * @name 2D access
+   * @{
+   */
+
+  storage_j& operator()(dimension, dimension);
+
+  storage_j& operator()(dimension, direction);
+  storage_j& operator()(direction, dimension);
+  storage_j& operator()(direction, direction);
+  /** @} */
+
+  /**
+   * @name 3D access
+   * @{
+   */
+  storage_j& operator()(dimension, dimension, dimension);
+
+  storage_j& operator()(direction, direction, direction);
+  storage_j& operator()(dimension, direction, direction);
+  storage_j& operator()(direction, dimension, direction);
+  storage_j& operator()(direction, direction, dimension);
+  storage_j& operator()(dimension, dimension, direction);
+  storage_j& operator()(dimension, direction, dimension);
+  storage_j& operator()(direction, dimension, dimension);
+  /** @} */
+
+  operator double() const;
+};
+
+/**
+ * @brief Dummy 1-dimensional storage
+ * @ingroup gridtools_clang
+ */
+struct storage_k : public storage {
+  storage_k();
+
+  storage_k& operator=(const storage&);
+  storage_k& operator+=(const storage&);
+  storage_k& operator-=(const storage&);
+  storage_k& operator/=(const storage&);
+  storage_k& operator*=(const storage&);
+
+  storage_k& operator()(int);
+
+  /**
+   * @name 1D access
+   * @{
+   */
+  storage_k& operator()(direction);
+  storage_k& operator()(dimension);
+  storage_k& operator()(offset);
+  /** @} */
+
+  /**
+   * @name 2D access
+   * @{
+   */
+
+  storage_k& operator()(dimension, dimension);
+
+  storage_k& operator()(dimension, direction);
+  storage_k& operator()(direction, dimension);
+  storage_k& operator()(direction, direction);
+  /** @} */
+
+  /**
+   * @name 3D access
+   * @{
+   */
+  storage_k& operator()(dimension, dimension, dimension);
+
+  storage_k& operator()(direction, direction, direction);
+  storage_k& operator()(dimension, direction, direction);
+  storage_k& operator()(direction, dimension, direction);
+  storage_k& operator()(direction, direction, dimension);
+  storage_k& operator()(dimension, dimension, direction);
+  storage_k& operator()(dimension, direction, dimension);
+  storage_k& operator()(direction, dimension, dimension);
+  /** @} */
+
+  operator double() const;
+};
+
+/**
+ * @brief Dummy 2-dimensional storage
+ * @ingroup gridtools_clang
+ */
+struct storage_ij : public storage {
+  storage_ij();
+
+  storage_ij& operator=(const storage&);
+  storage_ij& operator+=(const storage&);
+  storage_ij& operator-=(const storage&);
+  storage_ij& operator/=(const storage&);
+  storage_ij& operator*=(const storage&);
+
+  storage_ij& operator()(int, int);
+
+  /**
+   * @name 1D access
+   * @{
+   */
+  storage_ij& operator()(dimension);
+
+  storage_ij& operator()(direction);
+  storage_ij& operator()(offset);
+  /** @} */
+
+  /**
+   * @name 2D access
+   * @{
+   */
+
+  storage_ij& operator()(dimension, dimension);
+
+  storage_ij& operator()(dimension, direction);
+  storage_ij& operator()(direction, dimension);
+  storage_ij& operator()(direction, direction);
+  /** @} */
+
+  /**
+   * @name 3D access
+   * @{
+   */
+  storage_ij& operator()(dimension, dimension, dimension);
+
+  storage_ij& operator()(direction, direction, direction);
+  storage_ij& operator()(dimension, direction, direction);
+  storage_ij& operator()(direction, dimension, direction);
+  storage_ij& operator()(direction, direction, dimension);
+  storage_ij& operator()(dimension, dimension, direction);
+  storage_ij& operator()(dimension, direction, dimension);
+  storage_ij& operator()(direction, dimension, dimension);
+  /** @} */
+
+  operator double() const;
+};
+
+/**
+ * @brief Dummy 2-dimensional storage
+ * @ingroup gridtools_clang
+ */
+struct storage_ik : public storage {
+  storage_ik();
+
+  storage_ik& operator=(const storage&);
+  storage_ik& operator+=(const storage&);
+  storage_ik& operator-=(const storage&);
+  storage_ik& operator/=(const storage&);
+  storage_ik& operator*=(const storage&);
+
+  storage_ik& operator()(int, int);
+
+  /**
+   * @name 1D access
+   * @{
+   */
+  storage_ik& operator()(dimension);
+
+  storage_ik& operator()(direction);
+  storage_ik& operator()(offset);
+  /** @} */
+
+  /**
+   * @name 2D access
+   * @{
+   */
+
+  storage_ik& operator()(dimension, dimension);
+
+  storage_ik& operator()(dimension, direction);
+  storage_ik& operator()(direction, dimension);
+  storage_ik& operator()(direction, direction);
+  /** @} */
+
+  /**
+ * @name 3D access
+ * @{
+ */
+  storage_ik& operator()(dimension, dimension, dimension);
+
+  storage_ik& operator()(direction, direction, direction);
+  storage_ik& operator()(dimension, direction, direction);
+  storage_ik& operator()(direction, dimension, direction);
+  storage_ik& operator()(direction, direction, dimension);
+  storage_ik& operator()(dimension, dimension, direction);
+  storage_ik& operator()(dimension, direction, dimension);
+  storage_ik& operator()(direction, dimension, dimension);
+  /** @} */
+
+  operator double() const;
+};
+
+/**
+ * @brief Dummy 2-dimensional storage
+ * @ingroup gridtools_clang
+ */
+struct storage_jk : public storage {
+  storage_jk();
+
+  storage_jk& operator=(const storage&);
+  storage_jk& operator+=(const storage&);
+  storage_jk& operator-=(const storage&);
+  storage_jk& operator/=(const storage&);
+  storage_jk& operator*=(const storage&);
+
+  storage_jk& operator()(int, int);
+
+  /**
+   * @name 1D access
+   * @{
+   */
+  storage_jk& operator()(dimension);
+
+  storage_jk& operator()(direction);
+  storage_jk& operator()(offset);
+  /** @} */
+
+  /**
+   * @name 2D access
+   * @{
+   */
+
+  storage_jk& operator()(dimension, dimension);
+
+  storage_jk& operator()(dimension, direction);
+  storage_jk& operator()(direction, dimension);
+  storage_jk& operator()(direction, direction);
+  /** @} */
+
+  /**
+ * @name 3D access
+ * @{
+ */
+  storage_jk& operator()(dimension, dimension, dimension);
+
+  storage_jk& operator()(direction, direction, direction);
+  storage_jk& operator()(dimension, direction, direction);
+  storage_jk& operator()(direction, dimension, direction);
+  storage_jk& operator()(direction, direction, dimension);
+  storage_jk& operator()(dimension, dimension, direction);
+  storage_jk& operator()(dimension, direction, dimension);
+  storage_jk& operator()(direction, dimension, dimension);
+  /** @} */
+
+  operator double() const;
+};
+
+/**
+ * @brief Dummy 3-dimensional storage
+ * @ingroup gridtools_clang
+ */
+struct storage_ijk {
+  storage_ijk();
+
+  template <typename T>
+  storage_ijk(T...);
+
+  storage_ijk& operator=(const storage&);
+  storage_ijk& operator+=(const storage&);
+  storage_ijk& operator-=(const storage&);
+  storage_ijk& operator/=(const storage&);
+  storage_ijk& operator*=(const storage&);
+
+  storage_ijk& operator()(int, int, int);
+
+  /**
+   * @name 1D access
+   * @{
+   */
+  storage_ijk& operator()(direction);
+  storage_ijk& operator()(dimension);
+  storage_ijk& operator()(offset);
+  /** @} */
+
+  /**
+   * @name 2D access
+   * @{
+   */
+
+  storage_ijk& operator()(dimension, dimension);
+
+  storage_ijk& operator()(dimension, direction);
+  storage_ijk& operator()(direction, dimension);
+  storage_ijk& operator()(direction, direction);
+  /** @} */
+
+  /**
+   * @name 3D access
+   * @{
+   */
+  storage_ijk& operator()(dimension, dimension, dimension);
+
+  storage_ijk& operator()(direction, direction, direction);
+  storage_ijk& operator()(dimension, direction, direction);
+  storage_ijk& operator()(direction, dimension, direction);
+  storage_ijk& operator()(direction, direction, dimension);
+  storage_ijk& operator()(dimension, dimension, direction);
+  storage_ijk& operator()(dimension, direction, dimension);
+  storage_ijk& operator()(direction, dimension, dimension);
+  /** @} */
+
+  operator double() const;
+};
+
 struct var {
   var();
 
@@ -149,11 +533,11 @@ using meta_data_scalar_t = meta_data;
 
 #ifndef GRIDTOOLS_CLANG_STORAGE_T_DEFINED
 using storage_t = storage;
-using storage_ijk_t = storage;
-using storage_ij_t = storage;
-using storage_i_t = storage;
-using storage_j_t = storage;
-using storage_k_t = storage;
+using storage_ijk_t = storage_ijk;
+using storage_ij_t = storage_ij;
+using storage_i_t = storage_i;
+using storage_j_t = storage_j;
+using storage_k_t = storage_k;
 using storage_scalar_t = storage;
 #endif
 
