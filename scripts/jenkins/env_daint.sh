fa8abfe6aff4c46505ef455952e84da9a5de5201
cosunae <carlos.osuna@env.ethz.ch>
2018-06-06 10:12:33 +0200

    fix

--- scripts/jenkins/env_daint.sh
+++ scripts/jenkins/env_daint.sh
@@ -8,8 +8,10 @@ module swap gcc/5.3.0
 #export CXX=${LLVM_DIR}/bin/clang++
 #export CC=${LLVM_DIR}/bin/clang
 export BOOST_DIR=/project/c14/install/daint/boost/boost_1_67_0/
-module load /users/vogtha/modules/compilers/clang/3.8.1
-export LLVM_DIR="/users/vogtha/clang-3.8.1/"
-export CXX=${LLVM_DIR}/bin/clang++
-export CC=${LLVM_DIR}/bin/clang
+module load /project/c14/install/daint/clang/module_3.8.1
+export CXX=`which g++`
+export CC=`which gcc`
+
+#export CXX=${LLVM_DIR}/bin/clang++
+#export CC=${LLVM_DIR}/bin/clang
 
