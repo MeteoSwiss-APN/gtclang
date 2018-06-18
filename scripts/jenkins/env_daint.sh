9c5566d4e7272a98888003e98e1821bcce83ea22
carlos osuna <carlos.osuna@meteoswiss.ch>
2018-06-18 14:17:00 +0200

    remove comments

--- scripts/jenkins/env_daint.sh
+++ scripts/jenkins/env_daint.sh
@@ -5,16 +5,10 @@ module swap PrgEnv-cray PrgEnv-gnu
 module swap gcc/5.3.0
 module load cudatoolkit
 
-#LLVM_DIR=/scratch//snx3000/jenkins/workspace/clang/slave/daint/clang+llvm-3.8.1-x86_64-linux-gnu-ubuntu-16.04/
-#export CXX=${LLVM_DIR}/bin/clang++
-#export CC=${LLVM_DIR}/bin/clang
 export BOOST_DIR=/project/c14/install/daint/boost/boost_1_67_0/
 module load /project/c14/install/daint/clang/module_3.8.1
 export CXX=`which g++`
 export CC=`which gcc`
-
-#export CXX=${LLVM_DIR}/bin/clang++
-#export CC=${LLVM_DIR}/bin/clang
 
 export SLURM_RESOURCES=('--gres=gpu:1 -C gpu --account=c14')
 export SLURM_PARTITION="debug"
