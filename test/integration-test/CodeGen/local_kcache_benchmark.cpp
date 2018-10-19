ebb78218e6ba5aad1030acfb8e04b3e8db26949d
cosunae <carlos.osuna@meteoswiss.ch>
2018-10-19 12:31:46 +0200

    fix

--- test/integration-test/CodeGen/local_kcache_benchmark.cpp
+++ test/integration-test/CodeGen/local_kcache_benchmark.cpp
@@ -47,19 +47,26 @@ TEST(local_kcache, test) {
   verifier verif(dom);
 
   meta_data_t meta_data(dom.isize(), dom.jsize(), dom.ksize() + 1);
-  storage_t d_naive(meta_data, "d"), d_gt(meta_data, "d_gt"), c_gt(meta_data, "c_gt"), c_naive(meta_data, "c_naive");
+  storage_t a_gt(meta_data, "a_gt"), a_naive(meta_data, "a_naive"), b_naive(meta_data, "b"), b_gt(meta_data, "b_gt");
+  storage_t c_gt(meta_data, "c_gt"), c_naive(meta_data, "c_naive"), d_naive(meta_data, "d"), d_gt(meta_data, "d_gt");
 
+  verif.fillMath(8.0, 2.0, 1.5, 1.5, 2.0, 4.2, a_naive);
+  verif.fillMath(8.0, 2.0, 1.5, 1.5, 2.0, 4.2, a_gt);
+  verif.fillMath(5.8, 1.0, 1.7, 1.7, 1.9, 4.1, b_naive);
+  verif.fillMath(5.8, 1.0, 1.7, 1.7, 1.9, 4.1, b_gt);
+  verif.fillMath(7.8, 2.0, 1.1, 1.7, 1.9, 4.1, c_naive);
+  verif.fillMath(7.8, 2.0, 1.1, 1.7, 1.9, 4.1, c_gt);
   verif.fillMath(8.0, 2.0, 1.5, 1.5, 2.0, 4.0, d_naive);
   verif.fillMath(8.0, 2.0, 1.5, 1.5, 2.0, 4.0, d_gt);
-  verif.fillMath(7.8, 2.0, 1.1, 1.7, 1.9, 4.1, c_gt);
-  verif.fillMath(7.8, 2.0, 1.1, 1.7, 1.9, 4.1, c_naive);
 
-  OPTBACKEND::local_kcache local_kcache_gt(dom, d_gt, c_gt);
-  cxxnaive::local_kcache local_kcache_naive(dom, d_naive, c_naive);
+  OPTBACKEND::local_kcache local_kcache_gt(dom, a_gt, b_gt, c_gt, d_gt);
+  cxxnaive::local_kcache local_kcache_naive(dom, a_naive, b_naive, c_naive, d_naive);
 
   local_kcache_gt.run();
   local_kcache_naive.run();
 
-  ASSERT_TRUE(verif.verify(d_gt, d_naive));
+  ASSERT_TRUE(verif.verify(a_gt, a_naive));
+  ASSERT_TRUE(verif.verify(b_gt, b_naive));
   ASSERT_TRUE(verif.verify(c_gt, c_naive));
+  ASSERT_TRUE(verif.verify(d_gt, d_naive));
 }
