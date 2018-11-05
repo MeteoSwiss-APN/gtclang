8384aafa71a6fe78c9013885444cbad45a6b579b
cosunae <carlos.osuna@meteoswiss.ch>
2018-11-05 08:47:52 +0100

    fix

--- test/integration-test/CodeGen/kcache_fill_backward.cpp
+++ test/integration-test/CodeGen/kcache_fill_backward.cpp
@@ -21,10 +21,10 @@ stencil kcache_fill_backward {
   storage in, out;
 
   Do {
-    vertical_region(k_end, k_end - 1) {
-      out = in + in[k - 1] + in[k - 2];
+    vertical_region(k_end - 2, k_end - 2) {
+      out = in + in[k - 1] + in[k - 2] + in[k + 1] + in[k + 2];
     }
-    vertical_region(k_end - 2, k_start + 2) {
+    vertical_region(k_end - 3, k_start + 2) {
       out = in + in[k + 1] + in[k - 1] + in[k - 2] + out[k + 1];
     }
     vertical_region(k_start + 1, k_start + 1) {
