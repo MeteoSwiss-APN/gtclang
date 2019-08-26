fc79c57dada417b380118078bb4599fbd977555a
Lukas Mosimann <lukas.mosimann@cscs.ch>
2019-08-26 13:53:31 +0200

    Fix naming

--- src/gtclang/Frontend/GTClangASTConsumer.cpp
+++ src/gtclang/Frontend/GTClangASTConsumer.cpp
@@ -227,8 +227,8 @@ void GTClangASTConsumer::HandleTranslationUnit(clang::ASTContext& ASTContext) {
              stencilPair.second->Attributes.has(dawn::sir::Attr::AK_NoCodeGen)
                  ? ""
                  : DawnTranslationUnit->getStencils().at(
-                       DawnTranslationUnit->getStencils().count("Stencil") > 0
-                           ? "Stencil"
+                       DawnTranslationUnit->getStencils().count("<restored>") > 0
+                           ? "<restored>"
                            : stencilPair.second->Name)))
         context_->getDiagnostics().report(Diagnostics::err_fs_error) << dawn::format(
             "unable to replace stencil code at: %s", stencilDecl->getLocation().printToString(SM));
